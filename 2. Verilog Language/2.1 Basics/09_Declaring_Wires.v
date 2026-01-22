a7996f610a1da6d7ae2b6dfad23cd0bf451c10db
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-22 08:23:31 -0600

    Declare wires and implement logic in top_module

--- 2. Verilog Language/2.1 Basics/09_Declaring_Wires.v
+++ 2. Verilog Language/2.1 Basics/09_Declaring_Wires.v
@@ -1,1 +1,17 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    
+    wire a_b_and, c_d_and,or_temp;
+    assign a_b_and = a&b;
+    assign c_d_and = c&d;
+    assign or_temp = a_b_and | c_d_and;
+    assign out = or_temp;
+    assign out_n = ~or_temp; 
 
+endmodule
