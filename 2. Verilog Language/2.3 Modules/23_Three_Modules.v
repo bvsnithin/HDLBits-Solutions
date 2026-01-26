2c6cf755803643cc1023ff8bf073feff7521c4d9
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-26 14:51:10 -0600

    Implement three D flip-flops in top_module

--- 2. Verilog Language/2.3 Modules/23_Three_Modules.v
+++ 2. Verilog Language/2.3 Modules/23_Three_Modules.v
@@ -1,1 +1,8 @@
+module top_module ( input clk, input d, output q );
+	wire dff_out1, dff_out2;
+    
+    my_dff inst_1(clk, d, dff_out1);
+    my_dff inst_2(clk, dff_out1, dff_out2);
+    my_dff inst_3(clk, dff_out2, q);
+endmodule
 
