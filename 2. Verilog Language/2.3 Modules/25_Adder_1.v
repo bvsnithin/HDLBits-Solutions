60371925afa86f2ecc522cf211f3c9165b6a55e9
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-26 17:07:25 -0600

    Implement 32-bit adder using two 16-bit adders

--- 2. Verilog Language/2.3 Modules/25_Adder_1.v
+++ 2. Verilog Language/2.3 Modules/25_Adder_1.v
@@ -1,1 +1,11 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire [15:0] cout_1;
+    wire [15:0] cout_2;
+    add16 ( a[15:0], b[15:0], 15'b0, sum[15:0], cout_1 );
+    add16 ( a[31:16], b[31:16], cout_1, sum[31:16], cout_2 );
 
+endmodule
