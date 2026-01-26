99a19f2268d40d815697026120a9ac20db94906d
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-26 17:37:20 -0600

    Implement 32-bit adder/subtractor module

--- 2. Verilog Language/2.3 Modules/28_Adder_Subtractor.v
+++ 2. Verilog Language/2.3 Modules/28_Adder_Subtractor.v
@@ -1,1 +1,14 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    input sub,
+    output [31:0] sum
+);
+    wire [31:0] b_1;
+    wire cout_1, cout_2;
+    assign b_1 = b^{32{sub}}; 
+    add16 inst_1 ( a[15:0], b_1[15:0], sub, sum[15:0], cout_1 );
+    add16 inst_2 ( a[31:16], b_1[31:16], cout_1, sum[31:16], cout_2);
+endmodule
+
 
