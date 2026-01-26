b40c8c116081c40688dfa3a36226115d57a93c75
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-26 17:22:05 -0600

    Implement Carry Select Adder in Verilog

--- 2. Verilog Language/2.3 Modules/27_Carry_Select_Adder.v
+++ 2. Verilog Language/2.3 Modules/27_Carry_Select_Adder.v
@@ -1,1 +1,20 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cout1,cout2,cout3;
+    wire [15:0] sum1;
+    wire [15:0] sum2;
+    add16 add1(a[15:0],b[15:0],0,sum[15:0],cout1);
+    add16 add2(a[31:16],b[31:16],0,sum1,cout2);
+    add16 add3(a[31:16],b[31:16],1,sum2,cout3);
+    
+    always @(*) begin
+        case (cout1)
+            1'b0: sum[31:16] = sum1; 
+            1'b1: sum[31:16] = sum2;
+        endcase
+    end
+endmodule
 
