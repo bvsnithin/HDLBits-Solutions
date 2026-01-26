48d2e4d8989c36105e53b4c3ec742bffffe5b71c
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-26 17:02:57 -0600

    Add top_module with D flip-flops and multiplexer

--- 2. Verilog Language/2.3 Modules/24_Modules_And_Vectors.v
+++ 2. Verilog Language/2.3 Modules/24_Modules_And_Vectors.v
@@ -1,1 +1,26 @@
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output [7:0] q 
+);
+    wire [7:0] d_1;
+    wire [7:0] d_2;
+    wire [7:0] d_3;
+    my_dff8 inst_1( clk, d, d_1 );
+    my_dff8 inst_2( clk, d_1 , d_2 );
+    my_dff8 inst_3( clk, d_2 , d_3 );
+    
+    always @(*) begin
+        case(sel)
+            2'b00: q = d;
+            2'b01: q = d_1;
+            2'b10: q = d_2;
+            2'b11: q = d_3;
+        endcase
+            
+    end
+    
+
+endmodule
 
