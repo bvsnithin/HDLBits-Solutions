9bb38812944101d5e7ea1cf8753217ff749a2d6b
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-01-30 08:18:34 -0600

    Update 43_Generate_For_Loop_100_Digit_BCD_Adder.v

--- 2. Verilog Language/2.5 More Verilog Features/43_Generate_For_Loop_100_Digit_BCD_Adder.v
+++ 2. Verilog Language/2.5 More Verilog Features/43_Generate_For_Loop_100_Digit_BCD_Adder.v
@@ -1,1 +1,19 @@
+module top_module( 
+    input [399:0] a, b,
+    input cin,
+    output cout,
+    output [399:0] sum );
+    
+    wire[99:0] cout_temp;
+    bcd_fadd inst_1(a[3:0],b[3:0],cin,cout_temp[0],sum[3:0]);
+    
+    genvar i;
+    generate
+        for(i=1;i<100;i++) begin : genvarfor
+            bcd_fadd inst_2(a[i*4+3:i*4], b[i*4+3:i*4],cout_temp[i-1],cout_temp[i],sum[i*4+3:i*4]);
+        end
+    endgenerate
+    assign cout = cout_temp[99];
+
+endmodule
 
