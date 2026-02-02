9ded0d5f6be56eaa874fd9a8529d30ca7756de86
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-02 12:21:29 -0600

    Update 59_Gates_And_Vectors.v

--- 3. Circuits/3.1 Combinational Logic/3.1.1 Basic Gates/59_Gates_And_Vectors.v
+++ 3. Circuits/3.1 Combinational Logic/3.1.1 Basic Gates/59_Gates_And_Vectors.v
@@ -1,1 +1,29 @@
+module top_module( 
+    input [3:0] in,
+    output [2:0] out_both,
+    output [3:1] out_any,
+    output [3:0] out_different );
+    
+    always @(*) begin
+        out_both = 1'b0;
+        out_any = 1'b0;
+        out_different = 1'b0;
+        for(int i = 2;i>=0;i--) begin
+            out_both[i] = in[i+1]&in[i];
+        end
+        
+        for(int i = 3;i>=1;i--) begin
+            out_any[i] = in[i]|in[i-1];
+        end
+        
+        for(int i = 0;i<=3;i++) begin
+            if(i==3) begin
+                out_different[i] = in[3]^in[0];
+            end
+            else begin
+                out_different[i] = in[i]^in[i+1];
+            end
+        end
+    end
 
+endmodule
