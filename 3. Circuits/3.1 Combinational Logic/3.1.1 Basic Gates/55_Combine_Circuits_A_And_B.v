194e2cc6a3b5adf80202ec1c3c20ef0ca9c99b07
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-02 10:45:40 -0600

    Update 55_Combine_Circuits_A_And_B.v

--- 3. Circuits/3.1 Combinational Logic/3.1.1 Basic Gates/55_Combine_Circuits_A_And_B.v
+++ 3. Circuits/3.1 Combinational Logic/3.1.1 Basic Gates/55_Combine_Circuits_A_And_B.v
@@ -1,1 +1,10 @@
-
+module top_module (input x, input y, output z);
+	wire z1,z2,z3,z4;
+    assign z1 = (x ^ y) & x;
+    assign z2 = ~(x ^ y);
+    assign z3 = z1;
+    assign z4 = z2;
+    
+    assign z = ((z1|z2)^(z3&z4));
+    
+endmodule
