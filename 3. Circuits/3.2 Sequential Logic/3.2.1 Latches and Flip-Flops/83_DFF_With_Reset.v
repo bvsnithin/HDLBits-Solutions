5412d01e8eea6e7454e0925befdf3ea922eec185
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-03 23:37:45 -0600

    Update 83_DFF_With_Reset.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/83_DFF_With_Reset.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/83_DFF_With_Reset.v
@@ -1,1 +1,16 @@
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    
+    always @(posedge clk) begin
+        if(!reset) begin
+            q<=d;
+        end else begin
+            q<=0;
+        end
+    end
 
+endmodule
