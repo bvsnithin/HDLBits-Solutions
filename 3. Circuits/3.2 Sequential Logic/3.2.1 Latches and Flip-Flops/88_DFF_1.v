edeca8661314b5d1f9c44f3045cb426482389f32
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-07 14:29:55 -0600

    Update 88_DFF_1.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/88_DFF_1.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/88_DFF_1.v
@@ -1,1 +1,14 @@
+module top_module (
+    input clk,
+    input d, 
+    input ar,   // asynchronous reset
+    output reg q);
 
+    always @(posedge clk, posedge ar) begin
+        if(ar == 1) begin
+            q <= 0;
+        end
+        else q<=d;
+    end
+    
+endmodule
