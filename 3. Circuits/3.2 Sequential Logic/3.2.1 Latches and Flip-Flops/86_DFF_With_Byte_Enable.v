f9baad46ecbad7b06dbaeba61ac2be368f0a1b65
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-07 14:24:50 -0600

    Update 86_DFF_With_Byte_Enable.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/86_DFF_With_Byte_Enable.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/86_DFF_With_Byte_Enable.v
@@ -1,1 +1,25 @@
+module top_module (
+    input clk,
+    input resetn,
+    input [1:0] byteena,
+    input [15:0] d,
+    output [15:0] q
+);
+    
+    always @ (posedge clk) begin
+        if(!resetn) begin
+            q<=0;
+        end
+        else if(byteena ==2'b11) begin
+            q<=d;
+        end
+        else if(byteena == 2'b01) begin
+            q[7:0] <= d[7:0];
+        end
+        else if(byteena == 2'b10) begin
+            q[15:8] <= d[15:8];
+        end
+    end
+    
 
+endmodule
