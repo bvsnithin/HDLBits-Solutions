44d4ac805708647a5ce7ac10171b303c7f6bd8f0
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-08 13:03:54 -0600

    Update 93_DFFs_And_Gates.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/93_DFFs_And_Gates.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/93_DFFs_And_Gates.v
@@ -1,1 +1,22 @@
-
+module top_module (
+    input clk,
+    input x,
+    output z
+);
+    reg q1, q2, q3;
+    
+    wire d1, d2, d3;
+    
+    assign d1 = q1 ^ x;
+    assign d2 = ~q2 & x;
+    assign d3 = ~q3 | x;
+    
+    assign z = ~ (q1 | q2 | q3);
+    
+    always @(posedge clk) begin
+        q1 <= d1;
+        q2 <= d2;
+        q3 <= d3;
+    end
+             
+endmodule
