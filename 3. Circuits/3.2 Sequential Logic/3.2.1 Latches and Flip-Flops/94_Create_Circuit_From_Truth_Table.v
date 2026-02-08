baea238357279cb550bf9653ebf6d2970911e35c
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-08 13:07:08 -0600

    Update 94_Create_Circuit_From_Truth_Table.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/94_Create_Circuit_From_Truth_Table.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/94_Create_Circuit_From_Truth_Table.v
@@ -1,1 +1,21 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
 
+    always @(posedge clk) begin
+        if(j&k) begin
+            Q <= ~Q;
+        end
+        else if(~j & ~k) begin
+            Q <= Q;
+        end
+        else if (j & ~k) begin
+            Q <= 1;
+        end
+        else Q<= 0;
+           
+    end
+    
+endmodule
