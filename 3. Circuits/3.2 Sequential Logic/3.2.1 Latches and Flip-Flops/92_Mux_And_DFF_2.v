0efd62a3374e597940a6c31c8056e91b22732990
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-08 12:21:42 -0600

    Update 92_Mux_And_DFF_2.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/92_Mux_And_DFF_2.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/92_Mux_And_DFF_2.v
@@ -1,1 +1,15 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+    wire mux_1_out, mux_2_out;
+    
+    assign mux_1_out = E?w:Q;
+    assign mux_2_out = L?R:mux_1_out;
+    
+    always @(posedge clk) begin
+        Q <= mux_2_out;
+    end
 
+endmodule
