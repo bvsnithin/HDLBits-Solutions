bf5cd4765bd56c2b71a39e3a3f3a961d0659857e
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-08 13:23:36 -0600

    Update 95_Detect_An_Edge.v

--- 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/95_Detect_An_Edge.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.1 Latches and Flip-Flops/95_Detect_An_Edge.v
@@ -1,1 +1,12 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    reg [7:0] in_prev = 8'b0;
+    always @(posedge clk) begin
+        pedge = ~in_prev & in;
+        in_prev <= in;
+    end
 
+endmodule
