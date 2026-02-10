ed119aa728f3d1b1023374c7825dd545f1c53c19
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-10 11:27:51 -0600

    Add decade counter module with reset functionality
    Implement a decade counter that resets on reaching 9.

--- 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/100_Decade_Counter.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/100_Decade_Counter.v
@@ -1,1 +1,15 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if(reset == 1 || q == 4'b1001) begin
+            q <= 0;
+        end
+        else begin
+            q <= q + 4'b0001;
+        end
+    end
 
+endmodule
