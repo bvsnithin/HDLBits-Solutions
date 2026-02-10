136638219b47184c169907bcc9395b1d8d25f088
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-10 11:30:03 -0600

    Implement decade counter with reset functionality

--- 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/101_Decade_Counter_Again.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/101_Decade_Counter_Again.v
@@ -1,1 +1,15 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if(reset == 1 || q == 4'b1010) begin
+            q <= 1;
+        end
+        else begin
+            q <= q + 4'b0001;
+        end
+    end
 
+endmodule
