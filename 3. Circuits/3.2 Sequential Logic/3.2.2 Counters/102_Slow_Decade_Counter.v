489cdde56e6a0bea37e13b22d6bf9876a8de8ec8
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-10 11:46:39 -0600

    Implement slow decade counter in Verilog

--- 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/102_Slow_Decade_Counter.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/102_Slow_Decade_Counter.v
@@ -1,1 +1,21 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if(reset) q <= 0;
+        else if(slowena) begin
+            if(q == 9) begin
+                q <= 0;
+            end else begin
+                q <= q + 4'b0001;
+            end
+        end
+        else begin
+            q <= q;
+        end
+    end
 
+endmodule
