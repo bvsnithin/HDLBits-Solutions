6d3abe4d41cf23e3e59643d725fe12853f349841
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-12 12:25:03 -0600

    Implement 12-hour clock counter

--- 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/106_12_Hour_Clock.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.2 Counters/106_12_Hour_Clock.v
@@ -1,1 +1,66 @@
+module top_module(
+    input clk,
+    input reset,
+    input ena,
+    output pm,
+    output [7:0] hh,
+    output [7:0] mm,
+    output [7:0] ss); 
+    
+    
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            hh <= 8'h12;
+            mm <= 8'h00;
+            ss <= 8'h00;
+            pm <= 1'b0;
+        end
+        else if(ena) begin
+            if(ss == 8'h59) begin
+                
+                ss <= 8'h00;
+                
+                if (mm == 8'h59) begin
+                    
+                    mm <= 8'h00;
+                    
+                    if (hh == 8'h11) begin
+   						 hh <= 8'h12;
+    					 pm <= ~pm;        // toggle at 11 -> 12
+					end
+					else if (hh == 8'h12) begin
+    					hh <= 8'h01;      // 12 -> 1 (no toggle)
+					end
+					else begin
+    					if (hh == 8'h09)
+        					hh <= 8'h10;
+    					else
+        					hh <= hh + 8'h01;
+					end
+
+                end
+                else begin
+                    if (mm[3:0] == 4'd9) begin
+                    	mm[3:0] <= 4'd0;
+                    	mm[7:4] <= mm[7:4] + 4'd1;
+                	end
+                	else begin
+                   		mm[3:0] <= mm[3:0] + 4'd1;
+                	end
+                end
+            end
+            else begin
+                if (ss[3:0] == 4'd9) begin
+                	ss[3:0] <= 4'd0;
+                	ss[7:4] <= ss[7:4] + 4'd1;
+            	end
+            	else begin
+                	ss[3:0] <= ss[3:0] + 4'd1;
+            	end
+            end
+        end
+    end
+
+endmodule
 
