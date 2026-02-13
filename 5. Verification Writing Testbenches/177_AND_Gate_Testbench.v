3a0f5766b9b831c27a0ff1b27ddb92a337b5c605
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-13 17:09:22 -0600

    Create AND gate testbench in Verilog
    Added a testbench for the AND gate with various input combinations.

--- 5. Verification Writing Testbenches/177_AND_Gate_Testbench.v
+++ 5. Verification Writing Testbenches/177_AND_Gate_Testbench.v
@@ -1,1 +1,20 @@
-
+module top_module();
+	
+    logic [1:0] in;
+    logic out;
+    
+    andgate ANDGATE(.*);
+    
+    initial begin
+        in[1] = 0;
+        in[0] = 0;
+        #10;
+        in[0] = 1;
+        #10;
+        in[1] = 1;
+        in[0] = 0;
+        #10;
+        in[0] = 1;
+    end
+    
+endmodule
