b1e96a1c1cc06df5b210ba54ef391705aea01a35
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-13 16:33:50 -0600

    Add arithmetic operations and zero result check
    Implement addition and subtraction in top_module with zero check.

--- 4. Verification Reading Simulations/4.1 Finding bugs in code/163_Add_Sub.v
+++ 4. Verification Reading Simulations/4.1 Finding bugs in code/163_Add_Sub.v
@@ -1,1 +1,22 @@
+// synthesis verilog_input_version verilog_2001
+module top_module ( 
+    input do_sub,
+    input [7:0] a,
+    input [7:0] b,
+    output reg [7:0] out,
+    output reg result_is_zero
+);//
+    always @(*) begin
+        result_is_zero = 1;
+        case (do_sub)
+          0: out = a+b;
+          1: out = a-b;
+        endcase
+		
+        // Add reduction or operator
+        if (|out)
+            result_is_zero = 0;
+    end
+
+endmodule
 
