4bae68c31bb23a0230d9981e238621b95b110cde
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-13 15:48:45 -0600

    Implement synchronous reset for shift register

--- 3. Circuits/3.2 Sequential Logic/3.2.3 Shift Registers/113_Shift_Register_1.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.3 Shift Registers/113_Shift_Register_1.v
@@ -1,1 +1,24 @@
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+	
+    wire[2:0] q;
+    
+    always @(posedge clk) begin
+        if(!resetn) begin
+        	q <= 3'b0;
+			out <= 0;            
+        end
+        else begin
+            q[0] <= in;
+            q[1] <= q[0];
+            q[2] <= q[1];
+            out <= q[2];
+        end
+    end
+    
+endmodule
+
 
