720a5ac9f8659a5115be731a7da7875b65b97742
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-14 08:59:34 -0600

    Implement 174_Sequential_Circuit_10.v

--- 4. Verification Reading Simulations/4.2 Build a circuit from a simulation waveform/174_Sequential_Circuit_10.v
+++ 4. Verification Reading Simulations/4.2 Build a circuit from a simulation waveform/174_Sequential_Circuit_10.v
@@ -1,1 +1,27 @@
+module top_module (
+    input clk,
+    input a,
+    input b,
+    output q,
+    output state  );
+    
+    always @(posedge clk) begin
+        if(a==b) begin
+            state <= a;
+        end
+        else begin
+            state <= state;
+        end
+    end
+    
+    always @(*) begin
+        if(a==b) begin
+            q = state;
+        end
+        else begin
+            q = ~state;
+        end
+    end
+
+endmodule
 
