b238758bcac68dd8284f8084e3b808a37c1d8225
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-14 12:18:12 -0600

    Implement Rule 110 logic in top_module

--- 3. Circuits/3.2 Sequential Logic/3.2.4 More Circuits/118_Rule_110.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.4 More Circuits/118_Rule_110.v
@@ -1,1 +1,20 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q
+); 
+    reg [511:0] left;
+    assign left = {1'b0, q[511:1]};
+    reg [511:0] right;
+    assign right  = {q[510:0],1'b0};
+    always @(posedge clk) begin
+        if(load) begin
+            q <= data;
+        end
+        else begin
+            q <= (~(left) & right) | (q ^ right);
+        end
+    end
 
+endmodule
