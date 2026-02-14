ed0437a2c8e67418cfc773c5f8ef992115078d29
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-14 00:10:19 -0600

    Implement sequential circuit with state logic

--- 4. Verification Reading Simulations/4.2 Build a circuit from a simulation waveform/173_Sequential_Circuit_9.v
+++ 4. Verification Reading Simulations/4.2 Build a circuit from a simulation waveform/173_Sequential_Circuit_9.v
@@ -1,1 +1,13 @@
+module top_module (
+    input clk,
+    input a,
+    output [3:0] q );
+    
+    always @(posedge clk) begin
+        if(a) begin
+            q <= 4'b0100;
+        end
+        else q <= (q == 4'b0110) ? 4'b0000 : q + 4'b0001;
+    end
 
+endmodule
