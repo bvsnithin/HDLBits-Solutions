40eb5c94d26a5f95e65ae658b6f16102f395cb0f
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-14 09:27:23 -0600

    Add testbench for T Flip-Flop module

--- 5. Verification Writing Testbenches/179_T_Flip_Flop.v
+++ 5. Verification Writing Testbenches/179_T_Flip_Flop.v
@@ -1,1 +1,30 @@
-
+module top_module ();
+	
+    logic clk;
+    logic reset;
+    logic t;
+    logic q;
+    
+    tff TFF(.*);
+    
+    initial clk = 0;
+    always #5 clk = ~clk;
+    
+    initial begin
+        reset = 0;
+        t  = 0;
+        
+        @(posedge clk);
+        reset = 1;
+        t = 1;
+        
+        @(posedge clk);
+        reset = 0;
+        
+        repeat(2) @(posedge clk);
+        t = 0;
+    end
+    
+    
+    
+endmodule
