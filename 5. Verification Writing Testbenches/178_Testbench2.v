ce2bec2fed5b7dc09bb625951b40d8a9fcc6ab30
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-14 09:06:21 -0600

    Add testbench for q7 module with clock and inputs

--- 5. Verification Writing Testbenches/178_Testbench2.v
+++ 5. Verification Writing Testbenches/178_Testbench2.v
@@ -1,1 +1,35 @@
+module top_module();
+	logic clk;
+    logic in;
+    logic [2:0] s;
+    logic out;
+    
+    q7 Q7(.*);
+    
+    initial clk = 0;
+    always #5 clk = ~clk;
+    
+    initial begin
+        in = 0;
+        s = 3'd2;
+        
+        @(negedge clk);
+        s = 3'd6;
+        
+        @(negedge clk);
+        s = 3'd2;
+        in = 1'b1;
+        
+        @(negedge clk);
+        s = 3'd7;
+        in = 0;
+        
+        @(negedge clk);
+        in =1;
+        s = 3'd0;
+        
+        repeat(3) @(negedge clk);
+        in = 0;
+    end
+endmodule
 
