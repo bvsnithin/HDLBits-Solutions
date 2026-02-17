c6fa0736e1d1a37d5be5c2f0f013ccbb2ef57e59
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-17 13:38:31 -0600

    Update 120_Simple_FSM_1_Asynchronous_Reset.v

--- 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/120_Simple_FSM_1_Asynchronous_Reset.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/120_Simple_FSM_1_Asynchronous_Reset.v
@@ -1,1 +1,31 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
 
+    parameter A = 0, B = 1;
+    reg state, next_state;
+    
+    always @(*) begin
+        case(state)
+            A: begin
+                if(in==1) next_state = state;
+                else next_state = B;
+            end
+            B: begin
+                if(in==1) next_state = state;
+                else next_state = A;
+            end
+        endcase
+    end
+    
+    always @(posedge clk, posedge areset) begin
+        if(areset) begin
+            state <= B;
+        end else state <= next_state;
+    end
+    
+    assign out = (state==B);
+
+endmodule
