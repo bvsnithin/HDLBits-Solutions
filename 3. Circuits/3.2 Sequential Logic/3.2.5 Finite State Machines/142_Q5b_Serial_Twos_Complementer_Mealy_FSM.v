d818895a1fa83c759a62c3e032b9f71b3c71e80b
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-22 11:16:48 -0600

    Update 142_Q5b_Serial_Twos_Complementer_Mealy_FSM.v

--- 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/142_Q5b_Serial_Twos_Complementer_Mealy_FSM.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/142_Q5b_Serial_Twos_Complementer_Mealy_FSM.v
@@ -1,1 +1,30 @@
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+    parameter A = 0, B =1;
+    reg state, next_state;
+    always @(posedge clk, posedge areset) begin
+        if(areset) begin
+            state <= A;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+    
+    always @(*) begin
+        if(state == A) begin
+            next_state = x?B:A;
+            z = x;
+        end
+        else begin
+            next_state = B;
+            z = ~x;
+        end
+    end
+
+endmodule
 
