60f028e8168f2084ab2d2572265d6ac28b611d81
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-23 09:52:50 -0600

    Update 145_Q3c_FSM_Logic.v

--- 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/145_Q3c_FSM_Logic.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/145_Q3c_FSM_Logic.v
@@ -1,1 +1,25 @@
+module top_module (
+    input clk,
+    input [2:0] y,
+    input x,
+    output Y0,
+    output z
+);
+    reg [2:0] Y;
+    parameter y0 = 0, y1 = 1, y2 = 2, y3 = 3, y4 = 4;
+    
+    always @(*) begin
+        case(y)
+            y0: Y <= x?y1:y0;
+            y1: Y <= x?y4:y1;
+            y2: Y <= x?y1:y2;
+            y3: Y <= x?y2:y1;
+            y4: Y <= x?y4:y3;
+            default Y <= 3'b000;
+        endcase
+    end
+    
+    assign z = (y==y3) || (y==y4);
+    assign Y0 = Y[0];
 
+endmodule
