1883b55f4580a3a964b01375de47bbec93d88115
Nithin Bazaru <bvsnithin412@tamu.edu>
2026-02-23 21:36:42 -0600

    Update 134_PS_2_Packet_Parser.v

--- 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/134_PS_2_Packet_Parser.v
+++ 3. Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/134_PS_2_Packet_Parser.v
@@ -1,1 +1,29 @@
+module top_module(
+    input clk,
+    input [7:0] in,
+    input reset,
+    output done
+);
 
+	typedef enum logic [1:0] {B0, B1, B2, DONE} state_t;
+	state_t state, next_state;
+
+	always @(*) begin
+    	case(state)
+        	B0: next_state = in[3] ? B1 : B0;
+        	B1: next_state = B2;
+        	B2: next_state = DONE;
+            DONE: next_state = in[3]?B1:B0;
+    	endcase
+	end
+
+	always @(posedge clk) begin
+    	if(reset)
+        	state <= B0;
+    	else
+        	state <= next_state;
+	end
+
+	assign done = (state == DONE);
+
+endmodule
